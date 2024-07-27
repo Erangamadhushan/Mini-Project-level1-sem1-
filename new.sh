345246af00e75849180d558032c0f143c9b180c3
Erangamadhushan <erangamadhushann956@gmail.com>
2024-07-28 03:10:02 +0530

    create new css file for index4.html . it's called index4.css file and insert developers details and their github account names

--- new.sh
+++ new.sh
@@ -2,3 +2,7 @@
 # Author: [Eranga Madhushan]
 # Date: 2024-07-28
 # Description: This is description is used for new users to understand our project outlines
+
+#developers : Eranga Madhushan (Erangamadhushan)
+#developers : Themiya Kaluarachchi (Themiya)
+#developers : Thathsara abhimani (ThathAbhi)
