c65f816f79b7ea06eed87fadda7aa9a820da488d
Erangamadhushan <erangamadhushann956@gmail.com>
2024-09-04 11:16:34 +0530

    set developers details in collaborators.sh

--- Collaborators.sh
+++ Collaborators.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Author: [Eranga Madhushan]
 # Date: 2024-07-28
-# Description: This is description is used for new users to understand our project outlines
+# Description: This is description is used for visitors to understand our project outlines
 
 #developers : Eranga Madhushan (Erangamadhushan)
 #developers : Themiya Kaluarachchi (Themiya)
+#developers : Thareesha (Thareesha98)
+#developers : Ravindu Nishshanka (RC-inventrix)
 #developers : Thathsara abhimani (ThathAbhi)
+
