cc00dea040ad03fef5de7ff704141fb85a791f0b
Erangamadhushan <erangamadhushann956@gmail.com>
2024-09-04 18:56:28 +0530

    create about.html page and change index4.html page support and services phase inter face with bootstrap

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
         <link rel="icon" type="text/x-icon" href="image_video/main_logo1.png"/>
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"/>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
 
         <style>
             body{
