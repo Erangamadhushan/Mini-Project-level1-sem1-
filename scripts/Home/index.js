4ea642bca0d8ba2d4a30eb8f5898b0c0b7de645c
Erangamadhushan <erangamadhushann956@gmail.com>
2025-01-12 18:03:24 +0530

    Add section border styles and implement content for local and foreign visitors

--- scripts/Home/index.js
+++ scripts/Home/index.js
@@ -1,8 +1,10 @@
 import { textContent, img } from "./welcome.js";
+import { foreign, local } from "./whoyouare.js";
 
 window.addEventListener("DOMContentLoaded", () => {
     renderPageWelcomeSection(textContent, img);
     renderSubcribeSection();
+    renderRelativeSectionContent(foreign, local);
 });
 
 function renderPageWelcomeSection(textContent, img) {
@@ -19,7 +21,7 @@ function renderPageWelcomeSection(textContent, img) {
     `
     img.forEach((ele) => {
         welcomeTextContent += `
-            <div class="col-sm-4 py-2 welcomeImg">
+            <div class="col-sm-4 py-2 welcomeImg ">
                 <img src="${ele.src}" alt="${ele.alt}"/>
             </div>
         `
@@ -60,5 +62,58 @@ function renderSubcribeSection() {
     `
 }
 
+function renderRelativeSectionContent(foreign, local) {
+    console.log(foreign[0].desc[0]);
+    const relativeContainer = document.querySelector('.p_relative');
+    relativeContainer.innerHTML = `
+        <div class="container py-2 topic">
+            <h1 class="text-success">Welcome to Our Tourism Portal!</h1>
+            <p class="text-center">We recognize that visitors have different needs and preferences, so we've customized our content for both local and international travelers.</p>
+        </div>
+        <div class="container-fluid py-2 ">
+    `;
+    relativeContainer.innerHTML += `
+            <div class="row py-5 justify-content-center gap-3 py-2">
+                <div class="col-md-5 p-5 sectionborder">
+                    <h3>${foreign[0].topic}</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="${foreign[0].img}" alt="${foreign.topic}"/>
+                            <a href="#" class="btn btn-lg mt-5 btn-outline-success">${foreign[0].buttonText}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <ul>
+                                <li>${foreign[0].desc[0]}</li>
+                                <li>${foreign[0].desc[1]}</li>
+                                <li>${foreign[0].desc[2]}</li>
+                            </u>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 p-5 sectionborder">
+                    <h3>${local[0].topic}</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="${local[0].img}" alt="${local[0].topic}"/>
+                            <a href="#" class="btn btn-lg mt-5 btn-outline-success ">${local[0].buttonText}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <ul>
+                                <li>${local[0].desc[0]}</li>
+                                <li>${local[0].desc[1]}</li>
+                                <li>${local[0].desc[2]}</li>
+                            </u>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    
+    console.log(relativeContainer);
+
+    
+}
+
 /*
-*/
+*/ 
---
+++ scripts/Home/whoyouare.js
@@ -0,0 +1,25 @@
+export const foreign = [
+    {
+        topic:"For International Visitors:",
+        img:"./assets/images/Home/foreign.jpg",
+        desc:[
+            "We provide specially curated cultural experiences and tourist products designed for foreign visitors",
+            "Our content is tailored to help you better understand and explore our destination ",
+            "We consider your unique travel motivations and preferences to enhance your visit"
+        ],
+        buttonText:"Explorer More ..",
+    }
+]
+
+export const local = [
+    {
+        topic:"For Local Visitors:",
+        img:"./assets/images/Home/local_family.jpg",
+        desc:[
+            "Discover hidden gems and local experiences in your own backyard",
+            "Access exclusive local resident offers and events",
+            "Get insider tips and recommendations from our local experts"
+        ],
+        buttonText:"Explorer More .."
+    }
+]
