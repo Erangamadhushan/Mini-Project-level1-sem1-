dba673280534127cddd0a56556d26ec26772f902
Erangamadhushan <erangamadhushann956@gmail.com>
2025-01-14 03:45:33 +0530

    Add card hover effects and beach content; update styles for improved layout

--- scripts/Home/index.js
+++ scripts/Home/index.js
@@ -1,19 +1,22 @@
 import { textContent, img } from "./welcome.js";
 import { foreign, local } from "./whoyouare.js";
+import { beachSideContent, touristResourts} from './resource.js';
 
 
 window.addEventListener("DOMContentLoaded", () => {
     renderPageWelcomeSection(textContent, img);
-    //renderSubcribeSection();
+    renderExplorerDate();
     renderRelativeSectionContent(foreign, local);
     renderMainDescription();
     renderFoodStyles();
+    renderTouristResourts(touristResourts);
+    renderBeachSideContent(beachSideContent);
 });
 
 function renderMainDescription() {
     const mainDesc = document.querySelector('.mainDescription');
     mainDesc.innerHTML = `
-        <div class="row justify-content-center align-items-center">
+        <div class="row justify-content-center align-items-center" data-aos="fade-up" data-aos-duration="1000">
             <div class="col-md-7 grid place-items-center img_row" >
                 <div class="text_row box1 p-5" onload="myFunction1()" >
                     <h1 class="text-white mainName"><span>S</span>tart <span>t</span>o <span>F</span>inish</h1>
@@ -27,11 +30,11 @@ function renderMainDescription() {
 function renderFoodStyles() {
     const foodStylesContainer = document.querySelector('.foodStylesContainer');
     foodStylesContainer.innerHTML = `
-        <div class="row py-4">
+        <div class="row py-4" data-aos="fade-up" data-aos-duration="1000">
             <h2 class="text-center text-success">Sri Lankan Food Styles</h2>
             <p class="text-center text-success">Sri Lankan cuisine is characterized by its distinctive blend of herbs, spices, fish, vegetables, rice, and fruits. The culinary experience offers a complex interplay of flavors, featuring sweet caramelized onion relishes, bitter melon, spicy coconut, and carefully balanced curry heat.</p>
         </div>
-        <div class="row py-1 d-flex justify-content-center gap-3">
+        <div class="row py-1 d-flex justify-content-center gap-3" data-aos="fade-up" data-aos-duration="1000">
             <div class="col-md-3">
                 <img src="./assets/images/Home/foodStyle1.jpg" alt=""/>
             </div>
@@ -54,7 +57,7 @@ function renderPageWelcomeSection(textContent, img) {
     const welcometextContainer = document.querySelector('.welcometextContainer');
     welcometextContainer.innerHTML = " ";
     let  welcomeTextContent = `
-        <div class="row d-flex p-2 flex-column">
+        <div class="row d-flex p-2 flex-column" data-aos="fade-up" data-aos-duration="1000">
             <div class="py-5">
                 <h2 class="text-success font-bold text-center">${textContent[0]}</h2>
                 <p class="text-center text-xl">${textContent[1]}</p>
@@ -81,29 +84,21 @@ function renderPageWelcomeSection(textContent, img) {
 
 //render subscribtion section
 
-// function renderSubcribeSection() {
-//     const subscribeContainer = document.querySelector('.subscribeSection');
-//     subscribeContainer.innerHTML = `
-//         <div class="subscribe">
-//             <button type="button" id="closeBtn">&times;</button>
-//             <div>
-//                 <form action="subscribe.php" method="post">
-//                     <div class="mt-3">
-//                         <label for="userName" class="form-label text-white">User Name:</label>
-//                         <input type="text" id="userName" name="userName" class="form-control"/>
-//                     </div>
-//                     <div class="mt-3">
-//                         <label for="email" class="form-label text-white">Email :</label>
-//                         <input type="email" id="email" name="email" class="form-control"/>
-//                     </div>
-//                     <div class="d-grid mt-4">
-//                         <button type="submit" class="btn btn-warning">Subscribe</button>
-//                     </div>
-//                 </form>
-//             </div>      
-//         </div>
-//     `
-// }
+function renderExplorerDate() {
+    const explorerIslandContainer = document.querySelector('.explorerIsland');
+    explorerIslandContainer.innerHTML = `
+        <div class="row py-5" data-aos="fade-up" data-aos-duration="1000">
+            <div class="col-md-7 ">
+                <h2 class="text-xxl py-2">Explore Island Stories</h2>
+                <p class="text-lg py-2">Each place, and each smile in Sri Lanka has a story to tell. We have so much to share with you, so come along to our island in paradise!</p>
+                <p>Sri Lanka offers a diverse range of experiences for explorers, combining natural beauty, cultural heritage, and adventure opportunities:</p>
+            </div>
+            <div class="col-md-5 d-flex justify-content-center align-items-center">
+                <a href="https://the-shooting-star.com/offbeat-sri-lanka/" target="_blank" type="button" class="btn btn-outline-primary btn-lg">EXPLORE MORE..</a>
+            </div>
+        </div>
+   `;
+}
 
 function renderRelativeSectionContent(foreign, local) {
     console.log(foreign[0].desc[0]);
@@ -153,10 +148,45 @@ function renderRelativeSectionContent(foreign, local) {
         </div>
     `;
     
-    //console.log(relativeContainer);
-
     
 }
 
+function renderTouristResourts() {
+    const explorerTouristRosourtsContainer = document.querySelector('.explorerTouristResourts');
+    explorerTouristRosourtsContainer.innerHTML = " ";
+
+}
+function renderBeachSideContent(beachSideContent) {
+    const explorerBeachSideContainer = document.querySelector('.explorerBeachSides');
+    let explorerBeachSideContent = `
+        <div class="row py-5">
+            <div>
+                <h2 class="px-5">Explorer Beach Side</h2>
+                <p class="px-5">The country appears to have numerous noteworthy beaches, with various sources listing between 9 to 16 of the best beaches [4] [5]. Each beach destination offers unique experiences, though specific details about individual beaches are limited in the provided search results.</p>
+            </div>
+            <div class="row d-flex justify-content-around">
+            
+    `;
+    beachSideContent.forEach((ele) => {
+        explorerBeachSideContent += `
+            <div class="col-md-4 py-2" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                <div class="card">
+                    <img src="${ele.img}" alt="${ele.title}" class="card-img-top"/>
+                    <div class="card-body p-3">
+                        <h5 class="card-title text-center">${ele.title}</h5>
+                        <p class="card-text">${ele.desc}</p>
+                        <a href="#" class="btn btn-outline-primary">Explorer More ..</a>
+                    </div>
+                </div>
+            </div>
+        `
+    });
+    
+    explorerBeachSideContent += `</div></div> `;
+    explorerBeachSideContainer.innerHTML = explorerBeachSideContent;
+
+}
+
 /*
+This function is used to display the content of the foreign and local sections of the tourism portal.
 */ 
---
+++ scripts/Home/resource.js
@@ -0,0 +1,21 @@
+export const beachSideContent = [
+    {
+        title:"Hikkaduwa Beach",
+        img:"./assets/images/Home/beach1.jpg",
+        desc:"Hikkaduwa is a popular surf spot on the Sri Lankan south-west coast. It attracts thousands of surfers between October/November and March/April. There are: shallow sand-bottomed waves, A-frame sections, harbourside waves, mellow whitewash to practice the pop, and fast left-hander waves."
+    },
+    {
+        title:"Thalpe Beach Side",
+        img:"./assets/images/Home/beach2.jpg",
+        desc:"Thalpe Beach is a beach situated in the Southern province of Sri Lanka, close to the city of Galle. It is a popular destination among both locals and foreigners. There are vibrant shops, restaurant and various water sports related activities to do in the surrounding area of Thalpe."
+    }, 
+    {
+        title:"Jungle Beach Side",
+        img:"./assets/images/Home/beach3.jpg",
+        desc:"Jungle is a popular surf spot on the Sri Lankan south-west coast. It attracts thousands of surfers between October/November and March/April. There are: shallow sand-bottomed waves, A-frame sections, harbourside waves, mellow whitewash to practice the pop, and fast left-hander waves."
+    }
+];
+
+export const touristResourts = [
+
+];
