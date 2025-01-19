4028df0ca1650c13b079ae0bd64364e16980dd15
Erangamadhushan <erangamadhushann956@gmail.com>
2025-01-20 03:30:19 +0530

    Add search filter forms and enhance styles; improve search destination functionality and layout

--- scripts/searchDestination/filterForm.js
+++ scripts/searchDestination/filterForm.js
@@ -6,11 +6,41 @@ function renderFilterForm() {
     const filterContainer = document.getElementById('filterForm');
     let filterFormContent = `
         <div class="row">
+            <h1 class="text-center py-2">Search Destinations</h1>
+        </div>
+        <div class="row">
             <div class="col-md-6 relative">
                 <img src="./assets/images/searchDestination/visitor.png" alt="visitorImg"/>
             </div>
-            <div class="col-md-6">
-
+            <div class="col-md-6 flex flex-col">
+                <div>
+                    <form id="directfilter">
+                        <div class="row w-100">
+                            <div class="w-75">
+                                <input type="text" id="directInputField"/>
+                            </div>
+                            <div class="w-20">
+                                <button type="submit" id="directSubmitButton">Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="container">
+                    <form id="searchfiltering">
+                        <div class="py-3 w-80">
+                            <input type="text" id="province" class="form-control p-2"/>
+                        </div>
+                        <div class="py-3">
+                            <input type="text" id="district" class="form-control p-2"/>
+                        </div>
+                        <div class="py-3">
+                            <input type="text" id="city" class="form-control p-2"/>
+                        </div>
+                        <div class="py-3">
+                            <input type="submit" value="Search"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     `;
